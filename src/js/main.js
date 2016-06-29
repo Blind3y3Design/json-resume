3a618645c4004f10c655299bed51ffdb1078e0ab
Adam Sedwick <asedwick@situationuinteractive.com>
2016-06-29 14:07:52 -0500

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,52 @@
+// Custom Handlebars Helper
+Handlebars.registerHelper('if_eq', function(a, b, opts) {
+    if (a == b) {
+        return opts.fn(this);
+    } else {
+        return opts.inverse(this);
+    }
+});
+
+function ajax_get(url, callback) {
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            // console.log('responseText:' + xmlhttp.responseText);
+            try {
+                var data = JSON.parse(xmlhttp.responseText);
+            } catch(err) {
+                console.log(err.message + " in " + xmlhttp.responseText);
+                return;
+            }
+            callback(data);
+        }
+    };
+ 
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+ 
+ajax_get('/resume-data.json', function(data) {
+	console.log(data);
+ 
+    var source   = document.getElementById('resume-template').innerHTML;
+    var template = Handlebars.compile(source);
+    var html    = template(data);
+ 
+    document.getElementById("wrapper").innerHTML = html;
+});
+
+
+// Resume Template Code
+jQuery(document).ready(function($) {
+    /*======= Skillset *=======*/
+    $('.level-bar-inner').css('width', '0');
+    $(window).on('load', function() {
+        $('.level-bar-inner').each(function() {
+            var itemWidth = $(this).data('level');
+            $(this).animate({
+                width: itemWidth
+            }, 800);
+        });
+    });
+});
